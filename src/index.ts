fd32465ccd43de4100ed83c123f4baade2ce4513
hdangnguyen <hdangprod@gmail.com>
2023-02-15 11:05:47 +0700

    start server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-
-dotenv.config();
+import morgan from 'morgan';
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 3000;
 
+//log requests
+app.use(morgan('tiny'));
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
